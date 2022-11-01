4d4d2974197271718d9e371e3b6414280cf4270c
Moises Atay <macmoisesataypoma@gmail.com>
2022-11-01 10:01:19 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class Libro:
+    def __init__(self,id,titulo,genero,ISBN,editorial,autores):
+        self.id=id
+        self.titulo
+        self.titulo
+        self.genero
+        self.ISBN
+        self.editorial
+        self.autores
+
+
+
